c0f13cdd0e8651246a838b5c1fdd7ac20137cbb7
R3GR3TS <33294733+R3GR3TS@users.noreply.github.com>
2022-06-26 13:27:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Random dice = new Random();
+        int roll1 = dice.Next(1, 61);
+        int roll2 = dice.Next(1, 61);
+        int roll3 = dice.Next(1, 61);
+        int roll4 = dice.Next(1, 61);
+        int roll5 = dice.Next(1, 61);
+        int roll6 = dice.Next(1, 61);
+
+        Console.WriteLine($"Resultados: {roll1},{roll2},{roll3},{roll4},{roll5},{roll6}");
+    }
+}
